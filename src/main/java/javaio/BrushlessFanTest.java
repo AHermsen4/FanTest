3be13e346581299f08d5aedaf9d4c789aedf07e8
Alwin <AlwinHermsen@Outlook.com>
2023-10-10 13:45:34 +0200

    Nieuwe poging

---
+++ src/main/java/javaio/BrushlessFanTest.java
@@ -0,0 +1,25 @@
+package javaio;
+import com.pi4j.io.gpio.*;
+
+public class BrushlessFanTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Create a GPIO controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // Provision the pin
+        final GpioPinDigitalOutput fanPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Fan", PinState.LOW);
+
+        // Turn the fan on
+        fanPin.high();
+        System.out.println("Fan is ON");
+        Thread.sleep(5000); // Run for 5 seconds
+
+        // Turn the fan off
+        fanPin.low();
+        System.out.println("Fan is OFF");
+        
+        // Release resources
+        gpio.shutdown();
+    }
+}
